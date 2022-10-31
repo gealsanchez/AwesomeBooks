0e7d3c252c50d9f0a14462f7172f0cf0ca1b2f87
Amen TETTEH <tkamen@outlook.com>
2022-10-31 22:06:17 +0000

    Refactor books.js to separate data from logic

---
+++ data/books.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    Name: 'Anna Karenina',
+    Author: 'Leo Tolstoy',
+  },
+  {
+    Name: 'To Kill a Mockingbird',
+    Author: 'Harper Lee',
+  },
+  {
+    Name: 'The Great Gatsby',
+    Author: 'F. Scott Fitzgerald',
+  },
+  {
+    Name: 'One Hundred Years of Solitude',
+    Author: 'Gabriel García Márquez',
+  },
+  {
+    Name: 'A Passage to India',
+    Author: 'E.M. Forster',
+  },
+];
+export {books}
