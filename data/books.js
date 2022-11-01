fac1ddd28b8ae2fdcd3c7508f22865d6e951dc30
Amen TETTEH <tkamen@outlook.com>
2022-11-01 18:25:39 +0000

    Update books.js file

--- data/books.js
+++ data/books.js
@@ -1,23 +1,2 @@
-const books = [
-  {
-    Name: 'Anna Karenina',
-    Author: 'Leo Tolstoy',
-  },
-  {
-    Name: 'To Kill a Mockingbird',
-    Author: 'Harper Lee',
-  },
-  {
-    Name: 'The Great Gatsby',
-    Author: 'F. Scott Fitzgerald',
-  },
-  {
-    Name: 'One Hundred Years of Solitude',
-    Author: 'Gabriel García Márquez',
-  },
-  {
-    Name: 'A Passage to India',
-    Author: 'E.M. Forster',
-  },
-];
-export {books}
+const books = JSON.parse(localStorage.getItem('books')) || [];
+export {books};
