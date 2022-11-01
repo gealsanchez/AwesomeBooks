395c1c3a8bc2fb384cf81bf6d105b257120e24f6
Amen TETTEH <tkamen@outlook.com>
2022-11-01 18:23:51 +0000

    Update index.js file

--- js/index.js
+++ js/index.js
@@ -1,125 +1,94 @@
-//import {books} from '../data/books.js'
+import {books} from '../data/books.js';
 
-let books=JSON.parse(localStorage.getItem('books'))||[]
-
-
-const maincontainer = document.querySelector('.main');
-
-const createInput=(name,placeholder)=>{
-  const input = document.createElement('input');
-  input.setAttribute('type', 'text')
-  input.setAttribute('name', name)
-  input.setAttribute('placeholder', placeholder);
-  return input
-}
-
-const createTag=(tagName,textContent=null,className=null)=>{
+const createTag = (tagName, textContent = null, className = null) => {
   const tag = document.createElement(tagName);
   tag.textContent = textContent;
   tag.className = className;
-  return tag
-}
+  return tag;
+};
 
-const bookName = createInput('bookname','Title')
-const bookAuthor = createInput('bookauthor','Author')
+const booksTable = document.querySelector('#booksTable');
 
+const buttonAdd = document.querySelector('#addbutton');
 
-const title = createTag('h1','Awesome Books ','titleText')
-
-const booksArray = createTag('div',null,'booksArray')
-
-const buttonAdd = createTag('button','Add','buttonAdd')
-
-
-
-
-const br = document.createElement('br');
-const br1 = document.createElement('br');
-
-
-const createBookCard=(bookName,bookAuthor)=>{
-  const divName = createTag('div',null,'divName')
-  const divAuthor = createTag('div',null,'divAuthor')
-  const buttonRemove = createTag('button','Remove','buttonRemove')
+const createBookRow = (bookName, bookAuthor) => {
+  const divName = createTag('div', null, 'divName');
+  const divAuthor = createTag('div', null, 'divAuthor');
+  const buttonRemove = createTag('button', 'Remove', 'buttonRemove');
   const hr = document.createElement('hr');
-  const bookCard = createTag('div',null,'bookCard')
-  divName.textContent =bookName;
+  const bookRow = createTag('div', null, 'bookRow');
+  divName.textContent = bookName;
   divAuthor.textContent = bookAuthor;
 
-  const cardItems = [divName, divAuthor, buttonRemove, hr]
+  const cardItems = [divName, divAuthor, buttonRemove, hr];
 
   for (let j = 0; j < cardItems.length; j += 1) {
-    bookCard.appendChild(cardItems[j]);
+    bookRow.appendChild(cardItems[j]);
   }
 
-  buttonRemove.addEventListener("click",(event)=>{
-    let book =event.target.parentNode.getElementsByTagName('div'),
-    author=book[0].innerHTML,
-    title=book[1].innerHTML;
-    removeBook(author,title)
-    
-  }
+  buttonRemove.addEventListener('click', (event) => {
+    const target = event.target;
+    const book = target.parentNode.getElementsByTagName('div');
+    const author = book[0].innerHTML;
+    const title = book[1].innerHTML;
+    // Remove from data
+    removeBook(author, title);
+    // Remove from UI
+    removeBookFromUI(target);
+  });
+  return bookRow;
+};
 
-  )
-
-  return bookCard
-}
-
-const buildBookSection=(bookList)=>{
-  if(bookList&&bookList.length>0){
+const buildBookSection = (bookList) => {
+  if (bookList && bookList.length > 0) {
     for (let i = 0; i < bookList.length; i += 1) {
-      booksArray.appendChild(createBookCard(bookList[i].Name,bookList[i].Author));
-    }  
-    const content = [title, booksArray];  
-    for (let i = 0; i < content.length; i += 1) {
-      maincontainer.appendChild(content[i]);
-    }  
-  } 
+      booksTable.appendChild(
+          createBookRow(bookList[i].Name, bookList[i].Author),
+      );
+    }
+  }
+};
+
+const addBook = (name, author) => {
+  const books = JSON.parse(localStorage.getItem('books')) || [];
+  books.push({Name: name, Author: author});
+  localStorage.setItem('books', JSON.stringify(books));
+};
+
+const addBookToUI = (name, author) => {
+  console.log(booksTable);
+  booksTable.appendChild(createBookRow(name, author));
+};
+
+const removeBook = (name, author) => {
+  const books = JSON.parse(localStorage.getItem('books'));
+  const newBooks = books.filter((item, index, books) => {
+    return !((item.Name === name) & (item.Author === author));
+  });
+  localStorage.setItem('books', JSON.stringify(newBooks));
+};
+const removeBookFromUI = (item) => {
+  if (item.classList.contains('buttonRemove')) {
+    item.parentElement.remove();
+  }
+};
+
+const clearForm = () => {
+  document.querySelector('#booktitle').value = '';
+  document.querySelector('#bookauthor').value = '';
+};
+
+buttonAdd.addEventListener('click', (event) => {
+  const fields = document.querySelectorAll('input');
+  const title = fields[0].value;
+  const author = fields[1].value;
+  // Add to local storage
+  addBook(title, author);
+  addBookToUI(title, author);
+  clearForm();
+});
+
+// Display Books on page load
+if (books.length > 0) {
+  document.addEventListener('DOMContentLoaded', buildBookSection(books));
 }
-
-const buildForm =()=>{
-  const formItems=[bookName, br, bookAuthor, br1, buttonAdd]
-  for (let i = 0; i < formItems.length; i += 1) {
-    maincontainer.appendChild(formItems[i]);
-  }  
-}
-const addBook=(name,author)=>{
-  let books =JSON.parse(localStorage.getItem('books'))
-  books.push({Name:name,Author:author})
-  booksArray.innerHTML=''
-  let book={Name:name,Author:author}
-  books.push(book)
-  localStorage.setItem('books',JSON.stringify(books))
-  buildBookSection(books)
-  
-}
-const removeBook=(name,author)=>{
-  let books =JSON.parse(localStorage.getItem('books'))
-  let newBooks=books.filter((item,index,books)=>{
-    return (!(item.Name===name & item.Author===author))
-  })
-  booksArray.innerHTML=''
-  localStorage.setItem('books',JSON.stringify(newBooks))
-  buildBookSection(newBooks)
-}
-//buildBookSection(books)
-
-buttonAdd.addEventListener("click",(event)=>{
-  let fields=document.querySelectorAll('input'),  
-  title=fields[0].value,
-  author=fields[1].value;
-  console.log(title,author)
-  addBook(title,author)
-  fields[0].value=''
-  fields[1].value=''
-})
-
-
-buildBookSection(books)
-buildForm()
-
-
-
-
-
-
