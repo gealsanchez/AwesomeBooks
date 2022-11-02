be72d17a4b3e1bb622982421b76a63cf1fd3ae13
Amen TETTEH <tkamen@outlook.com>
2022-11-02 16:49:15 +0000

    Add book.js for Book class

---
+++ model/book.js
@@ -0,0 +1,35 @@
+class Book {
+  constructor(ID, Title, Author) {
+    this.ID = ID;
+    this.Title = Title;
+    this.Author = Author;
+  }
+
+  static getAll() {
+    const books = localStorage.getItem('books');
+    return (books === null) ? [] : JSON.parse(books);
+  }
+
+  add() {
+    const books = Book.getAll();
+    this.ID = Book.count() + 1;
+    books.push(this);
+    localStorage.setItem('books', JSON.stringify(books));
+    return this;
+  }
+
+  remove() {
+    const books = Book.getAll();
+    books.forEach((book, index) => {
+      if (parseInt(book.ID, 10) === parseInt(this.ID, 10)) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static count() {
+    return this.getAll().length;
+  }
+}
+export { Book };
